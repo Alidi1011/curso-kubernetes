0b59a2a20af8c740ff703ef741aaa23d15b79409
Alisson <arteaga10alisson@gmail.com>
2023-10-16 16:15:46 -0500

    add microservice cursos

---
+++ springboot-msvc-cursos/src/main/java/com/aarteaga/msvc/usuarios/SpringbootMsvcCursosApplication.java
@@ -0,0 +1,13 @@
+package com.aarteaga.msvc.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsvcCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsvcCursosApplication.class, args);
+	}
+
+}
---
+++ springboot-msvc-cursos/src/test/java/com/aarteaga/msvc/usuarios/SpringbootMsvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarteaga.msvc.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsvcCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
