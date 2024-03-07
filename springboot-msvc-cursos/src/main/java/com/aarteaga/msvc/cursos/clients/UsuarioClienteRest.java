b16a7f9daf76817ccf7e5fe479fe4d4ce8127491
Alisson <arteaga10alisson@gmail.com>
2024-03-06 19:22:32 -0500

    add changes

--- springboot-msvc-cursos/src/main/java/com/aarteaga/msvc/cursos/clients/UsuarioClienteRest.java
+++ springboot-msvc-cursos/src/main/java/com/aarteaga/msvc/cursos/clients/UsuarioClienteRest.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Iterator;
 import java.util.List;
 
-@FeignClient(name = "msvc-usuarios", url="localhost:8001")
+@FeignClient(name = "msvc-usuarios", url="msvc-usuarios:8001")
 public interface UsuarioClienteRest {
   @GetMapping("/{id}")
   Usuario detalle(@PathVariable Long id);
